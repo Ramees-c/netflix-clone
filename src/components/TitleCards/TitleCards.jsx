76f6b1ad69f30731d406e0c4ac8e04b4652b66d7
Ramees C <rameesc985@gmail.com>
2025-01-03 21:38:35 +0530

    more title card and footer component design created

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 import "./TitleCards.css";
 import cards_data from "../../assets/cards/Cards_data";
 
-function TitleCards() {
+function TitleCards({title, category}) {
   const cardsRef = useRef();
 
   const handleWheel = (e) => {
@@ -15,7 +15,7 @@ function TitleCards() {
   }, []);
   return (
     <div className="title-cards">
-      <h2>Popular on Netflix</h2>
+      <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
         {cards_data.map((card, index) => {
           return (
